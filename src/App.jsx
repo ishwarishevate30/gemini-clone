3bbef5bbe4646dde251217f3f87a4ac245eaefd5
Ishwari Shevate <ishwarishevate@gmail.com>
2025-12-26 11:39:32 +0530

    Fix Sidebar.css case sensitivity for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Sidebar from "./components/Sidebar/Sidebar";
 import Main from "./components/Main/Main";
-import "./components/Sidebar/sidebar.css";
+import "./components/Sidebar/Sidebar.css";
 import "./App.css";
 
 const App = () => {
