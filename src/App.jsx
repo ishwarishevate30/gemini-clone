675fae19c3338d53a4f746e7b97f460577c3f4c1
Ishwari Shevate <ishwarishevate@gmail.com>
2025-12-26 11:47:53 +0530

    Normalize line endings

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Sidebar from "./components/Sidebar/Sidebar";
 import Main from "./components/Main/Main";
-import "./components/Sidebar/Sidebar.css";
+
 import "./App.css";
 
 const App = () => {
